30a8c3dd5433d7ec9568c83c3d67712abbbbb32c
Aaron Primmer <aaronmprimmer@gmail.com>
2025-11-17 07:50:27 -0500

    Erased API

--- assets/js/weatherMain.js
+++ assets/js/weatherMain.js
@@ -1,16 +1,1 @@
-// weatherapi.com
-const API_KEY = "6fa45def5f924faba18122047251611";
-const WEATHER_LINK =
-  "https://api.weatherapi.com/v1/current.json?key=6fa45def5f924faba18122047251611";
-// EXAMPLE LINK https://api.weatherapi.com/v1/forecast.json?key=6fa45def5f924faba18122047251611&q=houston&days=3&aqi=no&alerts=no
 
-// fetch(`${WEATHER_LINK}&q=houston&days=3&aqi=no&alerts=no`)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (dataSet) {
-//     console.log(dataSet.current);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
