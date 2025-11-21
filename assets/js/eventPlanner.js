61b8d07b4244dad26e49e4171a7ac413c8697832
Aaron Primmer <aaronmprimmer@gmail.com>
2025-11-21 06:55:20 -0500

    Added favicon ideas and some notes in js

---
+++ assets/js/eventPlanner.js
@@ -0,0 +1,7 @@
+// TODO
+// Grab event input and search on serpapi for information & display
+// Save info to local storage for quick links
+// Do we want to use jquery and/or dayjs?
+// When user searches for destination, grab weather from weatherapi for current and 5 days
+// When user clicks on event, search for flight info to destination
+//
